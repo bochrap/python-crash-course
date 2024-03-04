e9c37738eeb98233f5b9513503b166dfaff8cdc6
bochrap <131978926+bochrap@users.noreply.github.com>
2024-03-04 20:38:29 +0000

    basics operations file

---
+++ test.py
@@ -0,0 +1,10 @@
+name = "Papiesz Polak  "
+quote = "Nie wiedziałem o pedofilii w kościele"
+message = (name.rstrip() + " powiedział: \n" + quote)
+
+print(message)
+print(1 + 7)
+print(100 - 92)
+print(int(16 / 2))
+print(4 * 2)
+
